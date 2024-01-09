3567a212d0b05a45d524ab8fc98052cab1d706bf
Alexey.K <lexxtlt@gmail.com>
2024-01-09 18:39:12 +0400

    init

---
+++ src/js/__tests__/orferByProps.test.js
@@ -0,0 +1,20 @@
+import orderByProps from "../app";
+
+test("Корректная сортировка", () => {
+    const obj = {
+      name: "мечник",
+      health: 10,
+      level: 2,
+      attack: 80,
+      deffence: 40,
+    };
+    const order = ["name", "level"];
+    const result = [
+      { key: "name", value: "мечник" },
+      { key: "level", value: 2 },
+      { key: "attack", value: 80 },
+      { key: "deffence", value: 40 },
+      { key: "health", value: 10 },
+    ];
+    expect(orderByProps(obj, order)).toEqual(result);
+  });
